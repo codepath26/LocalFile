67c355217bba564040730546cc0538d5c1d3f6b2
parth thakor <parththakor2610@gmail.com>
2023-07-29 20:15:01 +0530

    modified using the array and object concept

--- index.js
+++ index.js
@@ -1,85 +1,111 @@
+// getting the Data From DOM
+ let expenses = [];
 let amount = document.getElementById("ExpenseAmount");
-  let description = document.getElementById("Discription");
-  let category = document.getElementById("Category");
-  let addExpense = document.getElementById("add-items");
-  let items = document.getElementById("items");
 
-  addExpense.addEventListener("submit", addData);
-  items.addEventListener("click", modified);
+let description = document.getElementById("Description");
+let category = document.getElementById("Category");
+let addExpense = document.getElementById("add-items");
+let items = document.getElementById("items");
 
-  // Retrieve data from local storage and populate the UI list on page load
-  window.addEventListener("load", () => {
-    const data = JSON.parse(localStorage.getItem("expenseData")) || [];
-    data.forEach((item) => createListItem(item));
-  });
-
-  function addData(e) {
-    e.preventDefault();
-    console.log(amount.value, description.value, category.value);
-    let itemData = {
-      amount: amount.value,
-      description: description.value,
-      category: category.value,
-    };
-    createListItem(itemData);
-
-    // Save data to local storage
-    let data = JSON.parse(localStorage.getItem("expenseData")) || [];
-    data.push(itemData);
-    localStorage.setItem("expenseData", JSON.stringify(data));
-
-    // Clear input fields after adding the item
-    amount.value = "";
-    description.value = "";
-    category.value = "";
+addExpense.addEventListener("submit", addData);
+items.addEventListener("click", modified);
+window.addEventListener("load" ,getdataFromLocalStorage);
+function getdataFromLocalStorage (){
+  const getdeatail = localStorage.getItem("expenses");
+  if(getdeatail){
+    expenses = JSON.parse(getdeatail);
+    DisplayData();
   }
 
-  function createListItem(itemData) {
-    let item = document.createElement("li");
-    item.classList = "expense-list w-75";
+}
+function addData(e) {
+  e.preventDefault();
+  // console.log(amount.value , description.value , category.value);
+  let item = document.createElement("li");
+  item.classList.add("list-group-item");
+ 
+  item.appendChild(
+    document.createTextNode(
+      `Amount :${amount.value}, Description :${description.value}, Category : ${category.value}`
+    )
+  );
+  let obj = {
+    Amount: amount.value,
+    Description: description.value,
+    Category: category.value,
+  };
+expenses.push(obj);
+
+
+  let deletButton = document.createElement("button");
+  deletButton.classList = "btn btn-sm btn-danger mx-1 float-end delete";
+  deletButton.appendChild(document.createTextNode("Delete"));
+  let editButton = document.createElement("button");
+  editButton.appendChild(document.createTextNode("Edit"));
+  editButton.classList = "btn btn-sm btn-success mx-1 float-end edit";
+  item.appendChild(editButton);
+  item.appendChild(deletButton);
+  items.appendChild(item);
+ addlocal();
+  amount.value = "";
+  description.value = "";
+  category.value = "Movies";
+}
+// Delete Button functionality;
+function modified(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("delete")) {
+    let li = e.target.parentElement;
+    let index = Array.from(li.parentNode.children).indexOf(li)
+   
+    if (index !== -1) {
+      expenses.splice(index, 1);
+    }
+    addlocal();
+    items.removeChild(li);
+  }
+  if (e.target.classList.contains("edit")) {
+    let li = e.target.parentElement;
+    let index = Array.from(li.parentNode.children).indexOf(li)
+
+    let expense = expenses[index];
+    amount.value = expense.Amount;
+    description.value = expense.Description;
+    category.value = expense.Category;
+    if (index !== -1) {
+      expenses.splice(index, 1);
+    }
+    addlocal();
+    items.removeChild(li);
+  }
+}
+
+// use to display the data from the array
+function DisplayData  (){
+  items.innerHTML = "";
+  expenses.forEach((expense)=>{
+    let item = document.createElement('li');
+    item.classList.add("list-group-item");
     item.appendChild(
       document.createTextNode(
-        `Amount: ${itemData.amount}, Description: ${itemData.description}, Category: ${itemData.category}`
+        `Amount : ${expense.Amount}, Description :${expense.Description}, Category : ${expense.Category}`
       )
-    );
-    let deleteButton = document.createElement("button");
-    deleteButton.classList = "btn btn-sm btn-danger m-1 float-end delete";
-    deleteButton.appendChild(document.createTextNode("Delete"));
+    )
+    let deletButton = document.createElement('button');
+    deletButton.classList = "btn btn-danger btn-sm float-end delete mx-1";
+    deletButton.appendChild(document.createTextNode("Delete"));
 
-    let editButton = document.createElement("button");
-    editButton.classList = "btn btn-sm btn-success m-1 float-end edit";
-    editButton.appendChild(document.createTextNode("Edit"));
-
+    let editButton = document.createElement('button');
+    editButton.classList = "btn btn-success btn-sm mx-1 edit float-end"
+    editButton.appendChild(document.createTextNode('Edit'));
     item.appendChild(editButton);
-    item.appendChild(deleteButton);
+    item.appendChild(deletButton);
     items.appendChild(item);
-  }
+  });
+  
+}
 
-  // Delete and Edit Button functionality;
-  function modified(e) {
-    e.preventDefault();
-    if (e.target.classList.contains("delete")) {
-      let li = e.target.parentElement;
-      items.removeChild(li);
-
-      // Update local storage after removing the item
-      let data = JSON.parse(localStorage.getItem("expenseData")) || [];
-      data = data.filter((item) => {
-        return item.amount !== li.textContent.split(",")[0].replace("Amount:", "").trim();
-      });
-      localStorage.setItem("expenseData", JSON.stringify(data));
-    }
-    if (e.target.classList.contains("edit")) {
-      let li = e.target.parentElement;
-      let dataString = li.textContent.trim();
-      let [amountValue, descriptionValue, categoryValue] = dataString
-        .replace(/Amount:|Description:|Category:/g, "")
-        .split(",");
-
-      amount.value = amountValue.trim();
-      description.value = descriptionValue.trim();
-      category.value = categoryValue.trim();
-
-      items.removeChild(li);
-    }
-  }
+// adding to local storage 
+function addlocal (){
+  localStorage.setItem("expenses" , JSON.stringify(expenses));
+}
